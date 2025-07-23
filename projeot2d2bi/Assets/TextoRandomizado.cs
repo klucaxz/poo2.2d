73e6e19615d77e16fb8b41c1e90e22a461a2ff29
amostradinhoborabil <klucasvicente341@gmail.com>
2025-07-23 16:23:09 -0300

    2307

---
+++ projeot2d2bi/Assets/TextoRandomizado.cs
@@ -0,0 +1,33 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class TextoRandomizado : MonoBehaviour
+{
+   public string novoTexto;
+   public TextMeshProUGUI UITexto;
+   
+   public int numero;
+
+
+   private void Start()
+   {
+      UITexto.text = novoTexto + " " + numero;
+   }
+
+
+   void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space) && numero > 0)
+      {
+         numero--;
+               UITexto.text = novoTexto + " " + numero;
+      }
+      
+      if (Input.GetKeyDown(KeyCode.Space) && numero > 0)
+      {
+         numero++;
+         UITexto.text = novoTexto + " " + numero;
+      }
+   }
+}
